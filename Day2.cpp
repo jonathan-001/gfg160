62e1c0ec983ac762177c5d84986c6fa3fcf4deb9
Anshuman Singh <162829197+jonathan-001@users.noreply.github.com>
2024-11-22 13:41:20 +0530

    Create Day2.cpp
    code file added

---
+++ Day2.cpp
@@ -0,0 +1,26 @@
+// https://www.geeksforgeeks.org/batch/gfg-160-problems/track/arrays-gfg-160/problem/second-largest3735
+
+
+class Solution {
+  public:
+    // Function returns the second
+    // largest elements
+    int getSecondLargest(vector<int> &arr) {
+        // Code Here
+        int n = arr.size();
+        if(n<2) return -1;
+        int largest = -1;
+        int secondlargest = -1;
+        
+        for(int i = 0;i<n;i++){
+            if(arr[i]>largest){
+                secondlargest = largest;
+                largest = arr[i];
+            }
+            else if(arr[i]>secondlargest && arr[i]!=largest){
+                secondlargest = arr[i];
+            }
+        }
+        return secondlargest;
+    }
+};
